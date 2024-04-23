86ba1197c36f8392ce58f0a3376c2eed50cfe577
whitebreadisu <jeremy.braden@gmail.com>
2024-04-23 14:03:13 -0500

    Wresters api complete without error handling

--- src/main/java/com/ringpro/ringannouncerapi/wrestlers/Wrestlers.java
+++ src/main/java/com/ringpro/ringannouncerapi/wrestlers/Wrestlers.java
@@ -7,10 +7,23 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+// This is the model class for the Wrestlers object.  An instance of the Wrestler class represents a wrestler object.
+// Wrestlers.java defines the Wrestlers model class and all the things you can do with the wrestler class
+// What can we do with the Wrestler class?  Not a ton... create an instance of the wrestler class with constructors,
+// getters and setters for the elements within the class, and @override that I don't really know what it does
+
+
+
+// @Entity is used in spring apps to declare 'hey man, this is like a record in a database we should treat it that way'
+// @Table points us to the SQL table containing records that are for the Wrestlers model class
 @Entity
-@Table(name = "tbl_wrestlers")
+@Table(name = "tbl_wrestlers")    
 public class Wrestlers {
 
+
+    // @Id is declaring 'hey the next variable of the Wrestler model class is the primary key within the associated database table'
+    // @column is used to let the Wrestlers model class know for each class variable which table column the variable correlates to
+    // example.... the java object of Wrestlers has Wrestlers.name which correlates to [tbl_wrestlers].[wrestler_name]
     @Id
     @Column(name = "wrestlerID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,7 +39,8 @@ public class Wrestlers {
     private String weight;
     @Column(name = "wrestler_notes")
     private String notes;
-    
+
+    // constructors
     public Wrestlers() {
     }
 
@@ -47,6 +61,7 @@ public class Wrestlers {
         this.notes = notes;
     }
 
+    //getters and setters
     public int getId() {
         return id;
     }
--- src/main/java/com/ringpro/ringannouncerapi/wrestlers/WrestlersService.java
+++ src/main/java/com/ringpro/ringannouncerapi/wrestlers/WrestlersService.java
@@ -2,6 +2,10 @@ package com.ringpro.ringannouncerapi.wrestlers;
 
 import org.springframework.data.jpa.repository.JpaRepository; 
 
+// this class is used for connecting the sql DB to interact with an instance of the Wrestlers model class
+// the 'extends' statement informs the class of its databaseness, allowing us to interact
+// with an instance of this class to do database things like create/modify/delete wrestler records
+
 public interface WrestlersService extends JpaRepository<Wrestlers, Integer>{
     
 }
